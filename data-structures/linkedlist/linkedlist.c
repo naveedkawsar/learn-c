f210319484208d7303f25595f4386685d8ce4a72
Naveed Kawsar <nkawsar@nkawsar-dev-debian.ventusnetworks.com>
2017-08-23 12:42:14 -0400

    Initial commit of C repository with template files for linkedlist implementation
    Signed-off-by: Naveed Kawsar <nkawsar@nkawsar-dev-debian.ventusnetworks.com>

---
+++ data-structures/linkedlist/linkedlist.c
@@ -0,0 +1,6 @@
+/*
+ * linkedlist.c
+ *
+ *  Created on: Aug 23, 2017
+ *      Author: nkawsar
+ */
---
+++ data-structures/linkedlist/linkedlist.h
@@ -0,0 +1,14 @@
+/*
+ * linkedlist.h
+ *
+ *  Created on: Aug 23, 2017
+ *      Author: nkawsar
+ */
+
+#ifndef LINKEDLIST_H_
+#define LINKEDLIST_H_
+
+
+
+#endif /* LINKEDLIST_H_ */
+
